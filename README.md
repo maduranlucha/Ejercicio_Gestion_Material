13d3ea0e8986c4ba3ebf50a5aad9c828af9775e4
miguel <maduranlucha@gmail.com>
2016-12-13 12:12:41 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+.checkout
+=========
+
+A Symfony project created on December 13, 2016, 12:03 pm.
---
+++ src/AppBundle/Entity/Material.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miguel
+ * Date: 13/12/16
+ * Time: 12:11
+ */
